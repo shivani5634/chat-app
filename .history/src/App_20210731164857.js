33759fb1b4c3e45cdc872dbac3ac12bb43ca711e
shivani5634 <shivanigupta5634769@gmail.com>
2021-07-31 17:13:11 +0530

    scaffolding chat-app

---
+++ .history/src/App_20210731164857.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles";
+
+function App() {
+  return <div>hello</div>;
+}
+
+export default App;
---
+++ .history/src/App_20210731165000.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles/main.scss";
+
+function App() {
+  return <div>hello</div>;
+}
+
+export default App;
